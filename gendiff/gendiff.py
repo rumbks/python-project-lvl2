7458819abb164e157aa61f24499d75e16a549efe
aukhadeev <rumbks@gmail.com>
2021-06-04 11:42:54 +0500

    Add draft implementation for flat jsons

---
+++ gendiff/gendiff.py
@@ -0,0 +1,32 @@
+import json
+from collections import defaultdict
+from pathlib import Path
+from typing import MutableMapping
+
+
+def _get_string_representation(diff_dict: MutableMapping[str, str]) -> str:
+    result = []
+    for key in sorted(diff_dict.keys()):
+        result += [f"  {item}" for item in diff_dict[key]]
+    lines = '\n'.join(result)
+    return f"{{\n{lines}\n}}"
+
+
+def generate_diff(file1: Path, file2: Path) -> str:
+    diff = defaultdict(list)
+    json1: dict = json.load(open(file1))
+    json2: dict = json.load(open(file2))
+    for key in json1:
+        if key in json2:
+            if json1[key] == json2[key]:
+                diff[key] += [f"  {key}: {json1[key]}"]
+            else:
+                diff[key] += [f"- {key}: {json1[key]}", f"+ {key}: {json2[key]}"]
+            json2.pop(key)
+            continue
+        diff[key] += [f"- {key}: {json1[key]}"]
+
+    for key in json2:
+        diff[key] += [f"+ {key}: {json2[key]}"]
+
+    return _get_string_representation(diff)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+from gendiff import generate_diff
 
 
 def main():
@@ -7,6 +8,8 @@ def main():
     parser.add_argument("second_file")
     parser.add_argument("-f", "--format", metavar="FORMAT", help="set format of output")
     args = parser.parse_args()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 
 if __name__ == '__main__':
