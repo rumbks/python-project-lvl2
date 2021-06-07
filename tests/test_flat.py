563dc654281f56f7c6eb4c50b78a8a4d1f8a7c1b
aukhadeev <rumbks@gmail.com>
2021-06-07 13:39:56 +0500

    Add test for empty json

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -1,4 +1,5 @@
 import json
+from io import StringIO
 from pathlib import Path
 
 from pytest import fixture
@@ -11,9 +12,11 @@ JSON1_PATH = FIXTURES / 'file1.json'
 
 JSON2_PATH = FIXTURES / 'file2.json'
 
+EMPTY_JSON_PATH = FIXTURES / 'empty.json'
 
-@fixture
-def expected_diff():
+
+@fixture()
+def expected_diff_for_json1_and_json2():
     return """{
   - follow: false
     host: hexlet.io
@@ -24,7 +27,21 @@ def expected_diff():
 }"""
 
 
-def test_flat(expected_diff):
-    diff = generate_diff(JSON1_PATH, JSON2_PATH)
-    assert diff == expected_diff
+@fixture()
+def expected_diff_for_json1_and_empty():
+    return """{
+  - follow: false
+  - host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+}"""
 
+
+def test_flat(expected_diff_for_json1_and_json2):
+    diff = generate_diff(JSON1_PATH, JSON2_PATH)
+    assert diff == expected_diff_for_json1_and_json2
+
+
+def test_flat_json1_and_empty(expected_diff_for_json1_and_empty):
+    diff = generate_diff(JSON1_PATH, EMPTY_JSON_PATH)
+    assert diff == expected_diff_for_json1_and_empty
