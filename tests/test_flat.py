760829623378102f768ad1e304792c96eafe2ff7
aukhadeev <rumbks@gmail.com>
2021-06-07 15:07:27 +0500

    Add tests for yaml format

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -1,22 +1,23 @@
-import json
-from io import StringIO
 from pathlib import Path
 
+import pytest
 from pytest import fixture
+
 from gendiff import generate_diff
 
 FIXTURES = Path.cwd() / 'tests' / 'fixtures'
 
-JSON1_PATH = FIXTURES / 'file1.json'
+JSON1_PATH = FIXTURES / "file1.json"
+JSON2_PATH = FIXTURES / "file2.json"
+EMPTY_JSON_PATH = FIXTURES / "empty.json"
 
-
-JSON2_PATH = FIXTURES / 'file2.json'
-
-EMPTY_JSON_PATH = FIXTURES / 'empty.json'
+YAML1_PATH = FIXTURES / "file1.yaml"
+YAML2_PATH = FIXTURES / "file2.yaml"
+EMPTY_YAML_PATH = FIXTURES / "empty.yaml"
 
 
 @fixture()
-def expected_diff_for_json1_and_json2():
+def expected_diff_for_file1_and_file2():
     return """{
   - follow: false
     host: hexlet.io
@@ -28,7 +29,7 @@ def expected_diff_for_json1_and_json2():
 
 
 @fixture()
-def expected_diff_for_json1_and_empty():
+def expected_diff_for_file1_and_empty():
     return """{
   - follow: false
   - host: hexlet.io
@@ -37,11 +38,20 @@ def expected_diff_for_json1_and_empty():
 }"""
 
 
-def test_flat(expected_diff_for_json1_and_json2):
-    diff = generate_diff(JSON1_PATH, JSON2_PATH)
-    assert diff == expected_diff_for_json1_and_json2
+@pytest.mark.parametrize(
+    "path_to_file1,path_to_file2", [(JSON1_PATH, JSON2_PATH), (YAML1_PATH, YAML2_PATH)]
+)
+def test_flat(path_to_file1, path_to_file2, expected_diff_for_file1_and_file2):
+    diff = generate_diff(path_to_file1, path_to_file2)
+    assert diff == expected_diff_for_file1_and_file2
 
 
-def test_flat_json1_and_empty(expected_diff_for_json1_and_empty):
-    diff = generate_diff(JSON1_PATH, EMPTY_JSON_PATH)
-    assert diff == expected_diff_for_json1_and_empty
+@pytest.mark.parametrize(
+    "path_to_file1,path_to_file2",
+    [(JSON1_PATH, EMPTY_JSON_PATH), (YAML1_PATH, EMPTY_YAML_PATH)],
+)
+def test_flat_file1_and_empty(
+    path_to_file1, path_to_file2, expected_diff_for_file1_and_empty
+):
+    diff = generate_diff(path_to_file1, path_to_file2)
+    assert diff == expected_diff_for_file1_and_empty
